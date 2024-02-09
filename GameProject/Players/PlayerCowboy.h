1586449a1bd371de68a0040d4e8157b3ca311509
Alexandr <Alexandr@KOMPUTER>
2024-02-09 18:19:11 +0300

    direction fix

--- GameProject/Players/PlayerCowboy.h
+++ GameProject/Players/PlayerCowboy.h
@@ -19,57 +19,56 @@ public:
     
     void move() override
     {
-    	time = clock.getElapsedTime().asMicroseconds();
+	    time = clock.getElapsedTime().asMicroseconds();
     	clock.restart();
     	time = time / 800;
-    	
-        if (direction==LEFT)
-		{
-			CurrentFrame += 0.02 * time;
-			if (CurrentFrame > 16) CurrentFrame -= 16;
-			sprite.setTextureRect(IntRect(100 * int(CurrentFrame)+10, 0, 60, 90));
-			sprite.move(-speed * time, 0);
-		}
+    	if (direction==LEFT)
+    	{
+    		CurrentFrame += 0.02 * time;
+    		if (CurrentFrame > 16) CurrentFrame -= 16;
+    		sprite.setTextureRect(IntRect(100 * int(CurrentFrame)+10, 0, 60, 90));
+    		sprite.move(-speed * time, 0);
+    	}
 
-		if (direction==RIGHT)
-		{
-			CurrentFrame += 0.02 * time;
-			if (CurrentFrame > 16) CurrentFrame -= 16;
-			sprite.setTextureRect(IntRect(100 * int(CurrentFrame)+20, 85, 60, 90));
-			sprite.move(speed * time, 0);
-		}
+    	if (direction==RIGHT)
+    	{
+    		CurrentFrame += 0.02 * time;
+    		if (CurrentFrame > 16) CurrentFrame -= 16;
+    		sprite.setTextureRect(IntRect(100 * int(CurrentFrame)+20, 85, 60, 90));
+    		sprite.move(speed * time, 0);
+    	}
 
-		if (direction==UP) 
-		{
-			CurrentFrame += 0.02 * time;
-			if (CurrentFrame > 16) CurrentFrame -= 16;
-			sprite.setTextureRect(IntRect(100 * int(CurrentFrame)+10, 265, 60, 90));
-			sprite.move(0, -speed * time);
-		}
+    	if (direction==UP) 
+    	{
+    		CurrentFrame += 0.02 * time;
+    		if (CurrentFrame > 16) CurrentFrame -= 16;
+    		sprite.setTextureRect(IntRect(100 * int(CurrentFrame)+10, 265, 60, 90));
+    		sprite.move(0, -speed * time);
+    	}
 
-		if (direction==DOWN)
-		{
-			CurrentFrame += 0.02 * time;
-			if (CurrentFrame > 16) CurrentFrame -= 16;
-			sprite.setTextureRect(IntRect(100 * int(CurrentFrame)+10, 175, 60, 90));
-			sprite.move(0, speed * time);
-		}
+    	if (direction==DOWN)
+    	{
+    		CurrentFrame += 0.02 * time;
+    		if (CurrentFrame > 16) CurrentFrame -= 16;
+    		sprite.setTextureRect(IntRect(100 * int(CurrentFrame)+10, 175, 60, 90));
+    		sprite.move(0, speed * time);
+    	}
 
-		if (direction==DOWN_LEFT)
-		{
-			CurrentFrame += 0.02 * time;
-			if (CurrentFrame > 16) CurrentFrame -= 16;
-			sprite.setTextureRect(IntRect(100 * int(CurrentFrame) + 10, 355, 60, 90));
-			sprite.move(-speed*time, speed * time);
-		}
+    	if (direction==DOWN_LEFT)
+    	{
+    		CurrentFrame += 0.02 * time;
+    		if (CurrentFrame > 16) CurrentFrame -= 16;
+    		sprite.setTextureRect(IntRect(100 * int(CurrentFrame) + 10, 355, 60, 90));
+    		sprite.move(-speed*time, speed * time);
+    	}
 
-		if (direction==DOWN_RIGHT)
-		{
-			CurrentFrame += 0.02 * time;
-			if (CurrentFrame > 16) CurrentFrame -= 16;
-			sprite.setTextureRect(IntRect(100 * int(CurrentFrame) + 10, 445, 65, 90));
-			sprite.move(speed * time, speed * time);
-		}
+    	if (direction==DOWN_RIGHT)
+    	{
+    		CurrentFrame += 0.02 * time;
+    		if (CurrentFrame > 16) CurrentFrame -= 16;
+    		sprite.setTextureRect(IntRect(100 * int(CurrentFrame) + 10, 445, 65, 90));
+    		sprite.move(speed * time, speed * time);
+    	}
     }
 
     void draw(RenderWindow& window)
@@ -78,43 +77,47 @@ public:
     }
     void Direction(Event& event)
     {
-	    if (Keyboard::isKeyPressed(sf::Keyboard::W) || Keyboard::isKeyPressed(sf::Keyboard::Up))
-	    {
-	    	pressed=true;
-	    	direction = UP;
-	    }
-    	if (Keyboard::isKeyPressed(sf::Keyboard::A) || Keyboard::isKeyPressed(sf::Keyboard::Left)
-    		&& !(Keyboard::isKeyPressed(sf::Keyboard::S)))
+    		if (Keyboard::isKeyPressed(sf::Keyboard::W) || Keyboard::isKeyPressed(sf::Keyboard::Up))
+    		{
+    			pressed=true;
+    			direction = UP;
+    		}
+    		if (Keyboard::isKeyPressed(sf::Keyboard::A) || Keyboard::isKeyPressed(sf::Keyboard::Left)
+				&& !(Keyboard::isKeyPressed(sf::Keyboard::S)))
+    		{
+    			pressed=true;
+    			direction = LEFT;
+    		}
+    		if (Keyboard::isKeyPressed(sf::Keyboard::D) || Keyboard::isKeyPressed(sf::Keyboard::Right)
+				&& !(Keyboard::isKeyPressed(sf::Keyboard::S)))
+    		{
+    			pressed=true;
+    			direction = RIGHT;
+    		}
+    		if (Keyboard::isKeyPressed(sf::Keyboard::S) || Keyboard::isKeyPressed(sf::Keyboard::Down)
+				&& !(Keyboard::isKeyPressed(sf::Keyboard::D))
+				&& !(Keyboard::isKeyPressed(sf::Keyboard::Right))
+				&& !(Keyboard::isKeyPressed(sf::Keyboard::A))
+				&& !(Keyboard::isKeyPressed(sf::Keyboard::Left)))
+    		{
+    			pressed=true;
+    			direction = DOWN;
+    		}
+    		if((Keyboard::isKeyPressed(Keyboard::Down) && (Keyboard::isKeyPressed(Keyboard::Left)))
+				|| (Keyboard::isKeyPressed(Keyboard::S) && (Keyboard::isKeyPressed(Keyboard::A))))
+    		{
+    			pressed=true;
+    			direction = DOWN_LEFT;
+    		}
+    		if((Keyboard::isKeyPressed(Keyboard::Down) && (Keyboard::isKeyPressed(Keyboard::Right)))
+				|| (Keyboard::isKeyPressed(Keyboard::S) && (Keyboard::isKeyPressed(Keyboard::D))))
+    		{
+    			pressed=true;
+    			direction = DOWN_RIGHT;
+    		}
+    	if(event.type==Event::KeyReleased)
     	{
-    		pressed=true;
-    		direction = LEFT;
-    	}
-    	if (Keyboard::isKeyPressed(sf::Keyboard::D) || Keyboard::isKeyPressed(sf::Keyboard::Right)
-			&& !(Keyboard::isKeyPressed(sf::Keyboard::S)))
-    	{
-    		pressed=true;
-    		direction = RIGHT;
-    	}
-    	if (Keyboard::isKeyPressed(sf::Keyboard::S) || Keyboard::isKeyPressed(sf::Keyboard::Down)
-			&& !(Keyboard::isKeyPressed(sf::Keyboard::D))
-			&& !(Keyboard::isKeyPressed(sf::Keyboard::Right))
-			&& !(Keyboard::isKeyPressed(sf::Keyboard::A))
-			&& !(Keyboard::isKeyPressed(sf::Keyboard::Left)))
-    	{
-    		pressed=true;
-    		direction = DOWN;
-    	}
-    	if((Keyboard::isKeyPressed(Keyboard::Down) && (Keyboard::isKeyPressed(Keyboard::Left)))
-			|| (Keyboard::isKeyPressed(Keyboard::S) && (Keyboard::isKeyPressed(Keyboard::A))))
-    	{
-    		pressed=true;
-    		direction = DOWN_LEFT;
-    	}
-    	if((Keyboard::isKeyPressed(Keyboard::Down) && (Keyboard::isKeyPressed(Keyboard::Right)))
-			|| (Keyboard::isKeyPressed(Keyboard::S) && (Keyboard::isKeyPressed(Keyboard::D))))
-    	{
-    		pressed=true;
-    		direction = DOWN_RIGHT;
+    		direction=STOP;
     	}
     	
     }
